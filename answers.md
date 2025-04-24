052ffe267dff5ad4e2517ca33f8ffb35b027ae5d
emilyaymond <emilydrew16@yahoo.com>
2025-04-24 14:42:20 -0500

    done

--- answers.md
+++ answers.md
@@ -2,17 +2,22 @@
 
 ## Answers
 
-**Name:**_________________________
-**Name:**_________________________
+**Name:** Emily Aymond
+**Name:** Camden Yale
 
 
 Place all written answers from `recitation-08.md` here for easier grading.
 
 
 
-- **1b)**
+- **1b)** The work is O((V + E) log V).
+  - The span is O(log V * D)
 
 
 
-- **2b)**
+- **2b)** graph = get_sample_graph()
+parents = bfs_path(graph, 's')
+paths = get_path(parents, 'd')
+print(paths)
+- output: sbc
 
--- main.py
+++ main.py
@@ -12,8 +12,26 @@ def shortest_shortest_path(graph, source):
       a dict where each key is a vertex and the value is a tuple of
       (shortest path weight, shortest path number of edges). See test case for example.
     """
-    ### TODO
-    pass
+    def shortest_path_helper(visited, front):
+        if len(front) == 0:
+            return visited
+
+        else:
+            weighted_distance, edges, node = heappop(front)
+            if node in visited:
+                return shortest_path_helper(visited, front)
+            else:
+                visited[node] = (weighted_distance, edges)
+                for neighbor, weight in graph[node]:
+                    heappush(front, (weighted_distance + weight, edges + 1, neighbor))
+                return shortest_path_helper(visited, front)
+
+    front = []
+    heappush(front, (0, 0, source))
+    visited = dict()
+    return shortest_path_helper(visited, front)
+
+
     
 
     
@@ -24,8 +42,23 @@ def bfs_path(graph, source):
       a dict where each key is a vertex and the value is the parent of 
       that vertex in the shortest path tree.
     """
-    ###TODO
-    pass
+    visited = set()
+    front = deque([source])
+    parent = {}
+    visited.add(source)
+
+    while front:
+        node = front.popleft()
+        for i in graph[node]:
+            if i not in visited:
+                visited.add(i)
+                parent[i] = node
+                front.append(i)
+    return parent
+
+
+
+
 
 def get_sample_graph():
      return {'s': {'a', 'b'},
@@ -43,6 +76,16 @@ def get_path(parents, destination):
       The shortest path from the source node to this destination node 
       (excluding the destination node itself). See test_get_path for an example.
     """
-    ###TODO
-    pass
+    path = []
+    while destination in parents:
+        parent = parents[destination]
+        path.append(parent)
+        destination = parent
+    return ''.join(reversed(path))
+
+
+graph = get_sample_graph()
+parents = bfs_path(graph, 's')
+paths = get_path(parents, 'd')
+print(paths)
 
